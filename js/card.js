f61eaea2f05a4eb154fcff1a92cbbc8d0e5b630a
Tommy Magnusson <tommagnuss@gmail.com>
2016-12-11 22:23:17 -0500

    added proper rows dynamically

--- js/card.js
+++ js/card.js
@@ -58,6 +58,29 @@ class ArticleTeaser {
 
 }
 
+// TODO: Somehow not displaying the actual decks... debug more
+
+function placeDecks(teasers) {
+  // place for the decks
+  let deckSpot = $(".article-decks"); // spot on the html to insert the decks
+  var currentDeck = []; // holds the current deck of cards (two for now)
+
+  for(let i = 0; i < teasers.length; i++) {
+    currentDeck.push(teasers[i]);
+    if (i % 2 != 0 || i +1 == teasers.length) {
+      // create a new deck element
+      let deckElement = $("<div></div>");
+      deckElement.addClass("row");
+      deckElement.addClass("deck-row");
+      
+      deckElement.append(currentDeck.map(c => { return c.makeCard(); })) // connect the deck with the row
+      deckSpot.append(deckElement); // connect the deck with the spot
+      currentDeck = []; // evacuate currentDeck
+    }
+  }
+
+}
+
 // Called when the document is loaded, courtesy of jQuery
 $(() => {
 
@@ -71,8 +94,6 @@ $(() => {
     imgAlt: "macbook",
     articleLink: "#"
    });
-  let deck = $(".deck-row")
-  deck.append(macbookTeaser.makeCard());
 
   let urbanizationTeaser = new ArticleTeaser({
     title: "How Urbanization Is Changing the Way We Look at Cities",
@@ -81,6 +102,15 @@ $(() => {
     imgAlt: "urbanization",
     articleLink: "#"
   });
-  deck.append(urbanizationTeaser.makeCard());
 
+  let watchMania = new ArticleTeaser({
+    title: "Watch Mania: Wrist Real Estate?",
+    teaserText: "They say time waits for no man. That may be true, but men often wait for Apple's new...",
+    imgUrl: "./img/watch.jpg",
+    imgAlt: "watch mania",
+    articleLink: "#"
+  })
+
+  let teasers = [macbookTeaser, urbanizationTeaser, watchMania];
+  placeDecks(teasers);
 });
